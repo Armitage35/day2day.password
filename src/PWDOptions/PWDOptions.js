e5c11c39b202b44505862cc3b7853b3304e50c45
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-04-01 19:25:43 +0000

    improve on look & feel

--- src/PWDOptions/PWDOptions.js
+++ src/PWDOptions/PWDOptions.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import './PWDOptions.css'
+import './PWDOptions.css';
+
 
 const PWDOptions = (props) => {
     return (
         <div className='PWDOptions'>
             <h2>Set your preferences</h2>
             <h4>Password length</h4>
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange" />
+            <input type='range' min='1' max='100' value='50' class='slider' id='myRange' />
             <h4>Advanced</h4>
+            <div className='advancedOptions'>
+                <div>
+                    <input type='checkbox' id='symbols' />
+                    <label for='symbols'>Include symbols</label>
+                </div>
+                <div>
+                    <input type='checkbox' id='numbers' />
+                    <label for='numbers'>Include numbers</label>
+                </div>
+                <div>
+                    <input type='checkbox' id='upperChar' />
+                    <label for='upperChar'>Include uppercase characters</label>
+                </div>
+                <div>
+                    <input type='checkbox' id='lowerChar' />
+                    <label for='lowerChar'>Include lowercase characters</label>
+                </div>
+            </div>
         </div>
     );
 };
--- src/PWDRender/PWDRender.js
+++ src/PWDRender/PWDRender.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './material-flat.css';
 
 const PWDRender = (props) => {
     return (
         <div>
+            <button class="bttn-material-flat bttn-md bttn-warning">Generate my next password</button>
         </div>
     )
 };
