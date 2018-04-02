99a7ceba8efcee24d6187a00b759c94d29ce6ec4
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-04-02 22:47:19 +0000

    add a chevron down to advanced options

--- src/PWDOptions/PWDOptions.js
+++ src/PWDOptions/PWDOptions.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const chevronDown = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"/></svg>;
+
 const PWDOptions = (props) => {
     return (
         <div className='PWDOptions'>
@@ -9,7 +11,7 @@ const PWDOptions = (props) => {
               <input className='rangeSliderRange' type='range' min='0' max='25' />
               <span className='rangeSliderValue'>0</span>
             </div>
-            <h4>Advanced</h4>
+            <h4>Advanced {chevronDown}</h4>
             <div className='advancedOptions'>
                 <div>
                     <input type='checkbox' id='symbols' />
